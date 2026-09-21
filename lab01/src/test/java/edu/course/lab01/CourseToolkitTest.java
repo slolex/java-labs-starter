00ee6c2dfa5fcf6d062871b964b863509d54b533
Максим Беликов <m692496@gmail.com>
2026-09-21 13:02:12 +0300

    Добавлен тест для 0.

--- lab01/src/test/java/edu/course/lab01/CourseToolkitTest.java
+++ lab01/src/test/java/edu/course/lab01/CourseToolkitTest.java
@@ -20,4 +20,11 @@ class CourseToolkitTest {
 
         assertFalse(result);
     }
+    @Test
+    void returnTrueForEvenNumberZero() {
+        boolean result = CourseToolkit.isEven(0);
+
+        assertTrue(result);
+    }
+
 }
