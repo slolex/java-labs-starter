ad56d6825f635e13b1ab809508e0708093de0dd2
Максим Беликов <m692496@gmail.com>
2026-09-21 14:01:39 +0300

    Добавлен метод isPrime, добавлены тесты для IsPrime.

--- lab01/src/main/java/edu/course/lab01/CourseToolkit.java
+++ lab01/src/main/java/edu/course/lab01/CourseToolkit.java
@@ -15,4 +15,16 @@ public final class CourseToolkit {
     public static boolean isEven(int number) {
         return number % 2 == 0;
     }
+
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= number; i = i + 1) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- lab01/src/test/java/edu/course/lab01/CourseToolkitTest.java
+++ lab01/src/test/java/edu/course/lab01/CourseToolkitTest.java
@@ -20,4 +20,44 @@ class CourseToolkitTest {
 
         assertFalse(result);
     }
+    @Test
+    void returnsTrueForNegativeEvenNumber() {
+        boolean result = CourseToolkit.isEven(-8);
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsFalseForNubmersLessTwo() {
+        boolean result = CourseToolkit.isPrime(1);
+        assertFalse(result);
+        result = CourseToolkit.isPrime(0);
+        assertFalse(result);
+        result = CourseToolkit.isPrime(-1);
+        assertFalse(result);
+    }
+
+    @Test
+    void returnsTrueForSimpleNumbers() {
+        boolean result = CourseToolkit.isPrime(2);
+        assertTrue(result);
+        result = CourseToolkit.isPrime(13);
+        assertTrue(result);
+        result = CourseToolkit.isPrime(19);
+        assertTrue(result);
+    }
+    @Test
+    public void returnsFalseForCompositeNumbers() {
+        boolean result = CourseToolkit.isPrime(6);
+        assertFalse(result);
+        result = CourseToolkit.isPrime(12);
+        assertFalse(result);
+    }
+
+    @Test
+    public void returnsFalseForSquares() {
+        boolean result = CourseToolkit.isPrime(144);
+        assertFalse(result);
+        result = CourseToolkit.isPrime(25);
+        assertFalse(result);
+    }
 }
